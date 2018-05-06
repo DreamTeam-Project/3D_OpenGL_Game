7511b023ba02934a2c08d0caf4259590d4afb71c
Ivan A. Tetyushkin <32076472+IvanTetyushkin@users.noreply.github.com>
2018-05-06 15:08:24 +0300

    Some physics, without logic

--- Physics.cpp
+++ Physics.cpp
@@ -1,80 +1,95 @@
-//#include "Physics.h"
-//
-//
-//
-//Physics_t::Physics_t() {
-//
-//	// Build the broadphase
-//	broadphase = new btDbvtBroadphase();
-//	// Set up the collision configuration and dispatcher
-//	collisionConfiguration = new btDefaultCollisionConfiguration();
-//	dispatcher = new btCollisionDispatcher(collisionConfiguration);
-//
-//	// The actual physics solver
-//	solver = new btSequentialImpulseConstraintSolver;
-//
-//	// The world.
-//	dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, broadphase, solver, collisionConfiguration);
-//	dynamicsWorld->setGravity(btVector3(0, -10.0f, 0));
-//
-//
-//
-//	// In this example, all monkeys will use the same collision shape : 
-//	// A box of 2m*2m*2m (1.0 is the half-extent !)
-//
-//
-//	//boxCollisionShape = new btBoxShape(btVector3(1.0f, 1.0f, 1.0f));
-//	//glm::quat orientation = glm::normalize(glm::quat(glm::vec3(180, 0, 90)));
-//	//motionstate = new btDefaultMotionState(
-//	//	btTransform(
-//	//		btQuaternion(orientation.x, orientation.y, orientation.z, orientation.w),
-//	//		btVector3(0.0f, 100.0f, 0.0f)
-//	//	)
-//	//);
-//	//btRigidBody::btRigidBodyConstructionInfo rigidBodyCI(
-//	//	1,                  // mass, in kg. 0 -> Static object, will never move.
-//	//	motionstate,
-//	//	boxCollisionShape,  // collision shape of body
-//	//	btVector3(0, 0, 0)    // local inertia
-//	//);
-//	//rigidBody = new btRigidBody(rigidBodyCI);
-//	//dynamicsWorld->addRigidBody(rigidBody);
-//
-//}
-//void Physics_t::step_do()
-//{
-//	dynamicsWorld->stepSimulation(1.0f / 60.0f, 10);
-//}
-//Physics_t::~Physics_t() {
-//	delete dynamicsWorld;
-//	delete solver;
-//	delete dispatcher;
-//	delete collisionConfiguration;
-//	delete broadphase;
-//	/*delete boxCollisionShape;
-//	delete motionstate;
-//	delete rigidBody;*/
-//}
-////btVector3 Physics::get_rig() {
-////	btVector3 mot;
-////	mot = rigidBody->getCenterOfMassPosition();
-////	return mot;
-////}
-////glm::vec3 get_glm_vec(btVector3 a) {
-////	glm::vec3 tmp;
-////
-////	tmp.x = a.getX();
-////	tmp.y = a.getY();
-////	tmp.z = a.getZ();
-////	return tmp;
-////}
-//
-//int Physics_t::addRigidBody(btRigidBody* rigidBody) {
-//	dynamicsWorld->addRigidBody(rigidBody);
-//	return 0;
-//}
-//
-//int Physics_t::removeRigidBody(btRigidBody* rigidBody) {
-//	dynamicsWorld->removeRigidBody(rigidBody);
-//	return 0;
-//}
+#include "Physics.h"
+
+void phys_body::set_velosity(btVector3& vel) {
+	body->setLinearVelocity(vel);
+}
+
+phys_body::phys_body(phys_world& world, btVector3& position, btVector3& col_shape, btScalar mass) {
+	btCollisionShape* colShape = new btBoxShape(col_shape);
+	world.collisionShapes.push_back(colShape);
+	/// Create Dynamic Objects
+	btTransform startTransform;
+	startTransform.setIdentity();
+
+	//rigidbody is dynamic if and only if mass is non zero, otherwise static
+	bool isDynamic = (mass != 0.f);
+
+	btVector3 localInertia(0, 0, 0);
+	if (isDynamic)
+		colShape->calculateLocalInertia(mass, localInertia);
+
+	startTransform.setOrigin(position);
+
+	//using motionstate is recommended, it provides interpolation capabilities, and only synchronizes 'active' objects
+	btDefaultMotionState* myMotionState = new btDefaultMotionState(startTransform);
+	btRigidBody::btRigidBodyConstructionInfo rbInfo(mass, myMotionState, colShape, localInertia);
+	body = new btRigidBody(rbInfo);
+	body->setCollisionFlags(body->getCollisionFlags() | btCollisionObject::CF_CUSTOM_MATERIAL_CALLBACK);
+	body->setUserPointer(body);
+
+	world.dynamicsWorld->addRigidBody(body);
+}
+
+phys_world::phys_world() {
+	collisionConfiguration = new btDefaultCollisionConfiguration();
+	dispatcher = new btCollisionDispatcher(collisionConfiguration);
+	overlappingPairCache = new btDbvtBroadphase();
+	solver = new btSequentialImpulseConstraintSolver;
+	dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, overlappingPairCache, solver, collisionConfiguration);
+	gContactAddedCallback = callbackFunc;
+}
+
+phys_world::~phys_world() {
+
+	for (int i = dynamicsWorld->getNumCollisionObjects() - 1; i >= 0; i--)
+	{
+		btCollisionObject* obj = dynamicsWorld->getCollisionObjectArray()[i];
+		btRigidBody* body = btRigidBody::upcast(obj);
+		if (body && body->getMotionState())
+		{
+			delete body->getMotionState();
+		}
+		dynamicsWorld->removeCollisionObject(obj);
+		delete obj;
+	}
+
+	//delete collision shapes
+	for (int j = 0; j < collisionShapes.size(); j++)
+	{
+		btCollisionShape* shape = collisionShapes[j];
+		collisionShapes[j] = 0;
+		delete shape;
+	}
+
+	//delete dynamics world
+	delete dynamicsWorld;
+
+	//delete solver
+	delete solver;
+
+	//delete broadphase
+	delete overlappingPairCache;
+
+	//delete dispatcher
+	delete dispatcher;
+
+	delete collisionConfiguration;
+}
+
+glm::vec3 phys_body::get_pos() {
+	btVector3 tmp = body->getCenterOfMassPosition();
+	return glm::vec3(tmp.getX(), tmp.getY(), tmp.getZ());
+}
+
+bool callbackFunc(btManifoldPoint& cp, btCollisionObjectWrapper* obj1, int id1, int index1, const btCollisionObjectWrapper* obj2, int id2, int index2) {
+	std::cout << "collision" << std::endl;
+	const btCollisionObject* tmp =obj1->getCollisionObject();
+	phys_body* newone = (phys_body*)tmp->getUserPointer();
+	//((bulletObject*)obj1->getUserPointer())->hit=true;
+	return false;
+}
+
+void phys_world::do_step(btScalar time) {
+	dynamicsWorld->stepSimulation(time);
+
+}
--- Physics.h
+++ Physics.h
@@ -11,137 +11,26 @@
 #include <glm/gtx/quaternion.hpp>
 #include <vector>
 
-//using std::vector;
-//using glm::vec3;
-//glm::vec3 get_glm_vec(const btVector3& a);
-//glm::vec3 get_glm_vec(btVector3 a);
-//
-//class Physics {
-//public:
-//	Physics ();
-//	~Physics ();
-//	void step_do();
-//	btVector3 get_rig();
-//private:
-//	btBroadphaseInterface * broadphase;
-//	btDefaultCollisionConfiguration* collisionConfiguration;
-//	btCollisionDispatcher* dispatcher;
-//	btSequentialImpulseConstraintSolver* solver;
-//	btDiscreteDynamicsWorld* dynamicsWorld;
-//	btConvexHullShape* shape;
-//	btCollisionShape* boxCollisionShape;
-//	btDefaultMotionState* motionstate;
-//	btRigidBody *rigidBody;
-//};
-//Physics::Physics () {
-////void Physics::print_cord() {
-////	btCollisionObject* obj = dynamicsWorld->getCollisionObjectArray()(1);
-////	btRigitBody* body = bt
-////}
-//Physics::Physics () {
-//	
-//	/*collisionConfiguration = new btDefaultCollisionConfiguration();
-//	broadphase = new btDbvtBroadphase();
-//	dispatcher = new btCollisionDispatcher(collisionConfiguration);
-//	solver = new btSequentialImpulseConstraintSolver;
-//
-//	dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, broadphase, solver, collisionConfiguration);
-//	dynamicsWorld->setGravity(btVector3(0, -1.01, 0));
-//
-//	boxCollisionShape = new btBoxShape(btVector3(1.0f, 1.0f, 1.0f));
-//	glm::quat orientation = glm::normalize(glm::quat(glm::vec3(180, 0, 90)));
-//	motionstate = new btDefaultMotionState(
-//		btTransform(
-//			btQuaternion(orientation.x, orientation.y, orientation.z, orientation.w), 
-//			btVector3(0.0f, 100.0f, 0.0f)
-//					)
-//	);
-//	btRigidBody::btRigidBodyConstructionInfo rigidBodyCI(
-//		1,                  // mass, in kg. 0 -> Static object, will never move.
-//		motionstate,
-//		boxCollisionShape,  // collision shape of body
-//		btVector3(0, 0, 0)    // local inertia
-//	);
-//	rigidBody = new btRigidBody(rigidBodyCI);
-//	dynamicsWorld->addRigidBody(rigidBody);
-//
-//}
-//void Physics::step_do()
-//{
-//	dynamicsWorld->stepSimulation(1.0f / 60.0f, 10);
-//}
-//	dynamicsWorld->setGravity(btVector3(0, -9.81f, 0));*/
-//	//end initialisation
-//	//start loading
-//
-//	/*btConvexHullShape* shape = new btConvexHullShape((const btScalar*)&(Pos.at(0)), num, sizeof(vec3));
-//	btVector3 color(1, 1, 1);
-//	btVector3 scaling(0.1, 0.1, 0.1);
-//	shape->setLocalScaling(scaling);*/
-//	/*btScalar mass(1);
-//	btDefaultMotionState* myMotionStage = new btDefaultMotionStage()*/
-//
-//	// Build the broadphase
-//	broadphase = new btDbvtBroadphase();
-//	// Set up the collision configuration and dispatcher
-//	collisionConfiguration = new btDefaultCollisionConfiguration();
-//	dispatcher = new btCollisionDispatcher(collisionConfiguration);
-//
-//	// The actual physics solver
-//	solver = new btSequentialImpulseConstraintSolver;
-//
-//	// The world.
-//	dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, broadphase, solver, collisionConfiguration);
-//	dynamicsWorld->setGravity(btVector3(0, -0.81f, 0));
-//
-//	// In this example, all monkeys will use the same collision shape : 
-//	// A box of 2m*2m*2m (1.0 is the half-extent !)
-//	boxCollisionShape = new btBoxShape(btVector3(1.0f, 1.0f, 1.0f));
-//	glm::quat orientation = glm::normalize(glm::quat(glm::vec3(180, 0, 90)));
-//	motionstate = new btDefaultMotionState(
-//		btTransform(
-//			btQuaternion(orientation.x, orientation.y, orientation.z, orientation.w), 
-//			btVector3(0.0f, 100.0f, 0.0f)
-//					)
-//	);
-//	btRigidBody::btRigidBodyConstructionInfo rigidBodyCI(
-//		1,                  // mass, in kg. 0 -> Static object, will never move.
-//		motionstate,
-//		boxCollisionShape,  // collision shape of body
-//		btVector3(0, 0, 0)    // local inertia
-//	);
-//	rigidBody = new btRigidBody(rigidBodyCI);
-//	dynamicsWorld->addRigidBody(rigidBody);
-//
-//}
-//void Physics::step_do()
-//{
-//	dynamicsWorld->stepSimulation(1.0f / 60.0f, 10);
-//}
-//Physics::~Physics () {
-//	delete dynamicsWorld;
-//	delete solver;
-//	delete dispatcher;
-//	delete collisionConfiguration;
-//	delete broadphase;
-//	delete boxCollisionShape;
-//	delete motionstate;
-//	delete rigidBody;
-//}
-//btVector3 Physics::get_rig() {
-//	btVector3 mot;
-//	mot = rigidBody->getCenterOfMassPosition();
-//	return mot;
-//}
-//
-//glm::vec3 get_glm_vec(const btVector3& a) {
-//glm::vec3 get_glm_vec(btVector3 a) {
-//	glm::vec3 tmp;
-//
-//	tmp.x = a.getX();
-//	tmp.y = a.getY();
-//	tmp.z = a.getZ();
-//	return tmp;
-//}
+class phys_world {
+public:
+	btAlignedObjectArray<btCollisionShape*> collisionShapes;
+	btDefaultCollisionConfiguration* collisionConfiguration; //= new btDefaultCollisionConfiguration();
+	btCollisionDispatcher* dispatcher;// = new btCollisionDispatcher(collisionConfiguration);
+	btBroadphaseInterface* overlappingPairCache;// = new btDbvtBroadphase();
+	btSequentialImpulseConstraintSolver* solver;// = new btSequentialImpulseConstraintSolver;
+	btDiscreteDynamicsWorld* dynamicsWorld;// = new btDiscreteDynamicsWorld(dispatcher, overlappingPairCache, solver, collisionConfiguration);
+	phys_world();
+	void do_step(btScalar time);
+	~phys_world();
+};
 
+class phys_body {
+	btRigidBody* body;
+
+	phys_body(phys_world& world, btVector3& position, btVector3& col_shape, btScalar mass);
+	void set_velosity(btVector3& vel);
+	glm::vec3 get_pos();
+};
+
+bool callbackFunc(btManifoldPoint& cp, const btCollisionObjectWrapper* obj1, int id1, int index1, const btCollisionObjectWrapper* obj2, int id2, int index2);
 #endif
